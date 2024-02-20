49a02c42e511fc18546fdc567e0bcbd8b6b44ca1
Kyle Smith <kyle.j.smith1996@gmail.com>
2024-02-19 22:54:25 -0600

    dropdowns select

--- src/components/Dropdowns.jsx
+++ src/components/Dropdowns.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { TextField } from "@mui/material";
+import {
+  TextField,
+  MenuItem,
+  FormControl,
+  Select,
+  InputLabel,
+} from "@mui/material";
 
 const Dropdowns = ({ category, variables, onChange, setState }) => {
   const handleVariableChange = (variable, value) => {
@@ -19,13 +25,51 @@ const Dropdowns = ({ category, variables, onChange, setState }) => {
 
   return (
     <div>
-      {Object.keys(variables).map((variableName) => (
-        <TextField
-          key={variableName}
-          label={variableName.charAt(0).toUpperCase() + variableName.slice(1)}
-          value={variables[variableName]}
-          onChange={(e) => handleVariableChange(variableName, e.target.value)}
-        />
+      {Object.entries(variables).map(([variableName, variableValue]) => (
+        <div key={variableName}>
+          {variableName === "variable2" && category === "harryPotter" ? (
+            <FormControl fullWidth>
+              <InputLabel>
+                {variableName.charAt(0).toUpperCase() + variableName.slice(1)}
+              </InputLabel>
+              <Select
+                value={variableValue}
+                onChange={(e) =>
+                  handleVariableChange(variableName, e.target.value)
+                }
+              >
+                <MenuItem value="Privet Drive">Privet Drive</MenuItem>
+                <MenuItem value="The Burrow">The Burrow</MenuItem>
+                <MenuItem value="Grimmauld Place">Grimmauld Place</MenuItem>
+              </Select>
+            </FormControl>
+          ) : variableName === "variable0" && category === "dynamicTable" ? (
+            <FormControl fullWidth>
+              <InputLabel>
+                {variableName.charAt(0).toUpperCase() + variableName.slice(1)}
+              </InputLabel>
+              <Select
+                value={variableValue}
+                onChange={(e) =>
+                  handleVariableChange(variableName, e.target.value)
+                }
+              >
+                <MenuItem value="boy">Boy</MenuItem>
+                <MenuItem value="girl">Girl</MenuItem>
+              </Select>
+            </FormControl>
+          ) : (
+            <TextField
+              label={
+                variableName.charAt(0).toUpperCase() + variableName.slice(1)
+              }
+              value={variableValue}
+              onChange={(e) =>
+                handleVariableChange(variableName, e.target.value)
+              }
+            />
+          )}
+        </div>
       ))}
     </div>
   );
